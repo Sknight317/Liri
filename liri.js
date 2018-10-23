e1c7345ce49aea3b3d05f392847babe87a15ee65
Sknight317 <Sknight23@mail.valenciacollege.edu>
2018-10-23 13:37:27 -0400

    got object to show

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+var request = require('request');
+
 // Using the require keyword lets us access all of the exports
 // in our ess.js file
 var stuff = require("./key");
@@ -10,5 +12,16 @@ console.log("ALL THE STUFF I NEED");
 console.log(stuff);
 console.log("--------------------------");
 
+
+
+var Spotify = require('node-spotify-api');
 // variable to access keys info
-var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(stuff.spotify);
+console.log(spotify);
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
